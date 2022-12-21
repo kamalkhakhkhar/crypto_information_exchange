527e86e57e12f8b0b5e2704b0a0f75bedf7cdd3c
Kamal <kamalkhakhkhar8@gmail.com>
2022-12-21 16:06:03 +0530

    Final Submission for first phase

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc ="https://avatars.githubusercontent.com/u/68731049";
+const avatarSrc1="https://avatars.githubusercontent.com/u/102232904?v=4";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are one of the best crypto informative app in India. 
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Kamal Khakhkhar  </Text>
+        </VStack>
+        
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc1} />
+          <Text> Jayesh Nakiya  </Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
